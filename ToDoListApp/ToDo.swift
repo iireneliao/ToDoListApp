67a9ec9cff4eb1c3fd93222c3bd7c90278e5cc35
iireneliao <iireneliao@gmail.com>
2021-07-20 13:12:05 -0700

    Initial Commit

---
+++ ToDoListApp/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoListApp
+//
+//  Created by Irene Liao on 7/20/21.
+//
+
+import UIKit
+
+class ToDo
+{
+    var name = "";
+    var important = false;
+}
---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Irene Liao on 7/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
